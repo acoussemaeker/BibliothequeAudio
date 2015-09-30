248cdb22408d3751442c41457b08478926b9a853
acoussemaeker <arnaud.cousco@hotmail.fr>
2015-10-01 00:15:19 +0200

    création du tableau pour la liste des fichier audio pour la biblio commune

--- index.php
+++ index.php
@@ -5,12 +5,20 @@
     include 'view/header.php';
 ?>
 
-    <body>
-<!--        <audio controls="controls" preload="none">
+<!--    <body>
+        <audio controls="controls" preload="none">
             <source src="audio/TEST.mp3" type="audio/mp3" />
             Votre navigateur n'est pas compatible
-        </audio>-->
+        </audio>
     </body>
-</html>
+</html>-->
     
-   
+    <table class="table table-hover tableListe">
+        <thead>
+            <tr class="info">
+                <th>Image</th>
+                <th>Titre</th>
+                <th>Lecteur</th>
+                <th></th>
+            </tr>
+        </thead>
